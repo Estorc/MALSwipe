b7d4812c1de55960dabec3a18f929dac50c80457
Estorc <loup.estorc@gmail.com>
2026-02-06 14:18:05 +0100

    Reimplements material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
-=======
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { SwipeComponent } from '../swipe/swipe.component';
@@ -13,4 +8,3 @@ export const routes: Routes = [
   { path: 'swipe', component: SwipeComponent },
   { path: '**', redirectTo: '/login' }
 ];
->>>>>>> 98c37a8a21de07ba905ba143b9758621d237748b
