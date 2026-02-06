bc5e79cbdb547ebbc2a9663824aa1af42babe159
ceb <lavallard.cebastien@gmail.com>
2026-02-06 15:04:22 +0100

    Ajout de la barre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; // <-- IMPORTANT
-import { Router } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { TabBarComponent } from './tab-bar/tab-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], // <-- c’est ça qu’on met pour router-outlet
-  templateUrl: './app.component.html'
+  // C'est ici qu'il faut ajouter TabBarComponent !
+  imports: [RouterOutlet, TabBarComponent], 
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   constructor(private router: Router) {}
